6c14a00990bf135a723a42ca272b3c88adf5551f
temiooo <temitope.joloko@gmail.com>
2017-09-20 00:24:05 +0100

    chore(travisCI): fix travisCI integration

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
+const config = require(`${__dirname}/../config/config.js`)[env];
 const db = {};
 
 let sequelize;
